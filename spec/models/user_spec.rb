d03ffaee05abd77bd2429a4bd9065dd554bf7da9
tomoron <tomoron326@outlook.jp>
2022-09-22 17:13:27 +0900

    spec:model user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "is valid with a first name, last name, email, and password" do
+    user = User.new(
+      first_name: "Aaron",
+      last_name: "Sumner",
+      email: "test@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze")
+    expect(user).to be_valid
+  end
+
+  it "is invalid without a first name" do
+    user = User.new(first_name: nil)
+    user.valid?
+    expect(user.errors[:first_name]).to include("can't be blank")
+  end
+
+  it "is invalid without a last name" do
+    user = User.new(last_name: nil)
+    user.valid?
+    expect(user.errors[:last_name]).to include("can't be blank")
+  end
+
+  it "is invalid without an email address" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it "is invalid with a duplicate email address" do
+    User.create(
+      first_name: 'Joe',
+      last_name: 'Tester',
+      email: "test@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze")
+    user = User.new(
+      first_name: 'Joe',
+      last_name: 'Tester',
+      email: "test@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze")
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+end
